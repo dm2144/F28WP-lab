e4d1fca7eaf9ae37a5bdd97e08173536eb30b771
salmaspc <ss2342@hw.ac.uk>
2023-10-16 23:53:09 +0400

    Update js.js

--- week4/js.js
+++ week4/js.js
@@ -1,7 +1,7 @@
 var OurRequest = new XMLHttpRequest();
 OurRequest.open('GET', 'https://salma-s-hw.github.io/F28WP-lab/cities1.json');
 OurRequest.onload = function(){
-var ourData = JSON.pars(ourRequest.responseText);
+var ourData = JSON.parse(OurRequest.responseText);
 console.log(ourData[0])
 };
 OurRequest.send();
