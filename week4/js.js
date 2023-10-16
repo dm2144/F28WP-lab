7f72fde265ae467098bdd947ecca27dac0708c01
Salma <144543200+Salma-S-HW@users.noreply.github.com>
2023-10-16 23:50:48 +0400

    Update js.js

--- week4/js.js
+++ week4/js.js
@@ -1,7 +1,7 @@
 var OurRequest = new XMLHttpRequest();
 OurRequest.open('GET', 'https://salma-s-hw.github.io/F28WP-lab/cities1.json');
 OurRequest.onload = function(){
-var ourData = OurRequest.responseText;
+var ourData = JSON.pars(ourRequest.responseText);
 console.log(ourData[0])
 };
 OurRequest.send();
