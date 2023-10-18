12827d7fd7f987fadc386e41e3987d8257f2af38
salmaspc <ss2342@hw.ac.uk>
2023-10-18 11:36:31 +0400

    add ajax

--- week4/js.js
+++ week4/js.js
@@ -1,7 +1,14 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://salma-s-hw.github.io/F28WP-lab/cities1.json');
-ourRequest.onload = function(){
-var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0])
-};
-ourRequest.send();
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://salma-s-hw.github.io/F28WP-lab/cities1.json');
+    ourRequest.onload = function(){
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0])
+    };
+    ourRequest.send();
+    
+
+});
+
+
