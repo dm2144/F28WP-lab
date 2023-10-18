b520fd6ea23c04a0c43765ca8e01992bf8274de8
salmaspc <ss2342@hw.ac.uk>
2023-10-18 11:44:38 +0400

    Update js.js

--- week4/js.js
+++ week4/js.js
@@ -1,14 +1,17 @@
+var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 'https://salma-s-hw.github.io/F28WP-lab/cities1.json');
     ourRequest.onload = function(){
     var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0])
+    renderHTML(ourData);
     };
     ourRequest.send();
-    
-
 });
 
+function renderHTML(data) {
+cityContainer.insertAdjacentHTML('beforeend', 'testing 123' );
 
+
+}
