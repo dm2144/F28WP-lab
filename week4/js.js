9b5b69c1560ac88c1304d8d9a0d35f7db7b036ad
salmaspc <ss2342@hw.ac.uk>
2023-10-19 12:39:42 +0400

    Update js.js

--- week4/js.js
+++ week4/js.js
@@ -14,7 +14,8 @@ function renderHTML(data) {
     var htmlString = "";
 
     for (i = 0; i<data.length; i++){
-        htmlString += "<p>" + data[i].name + "is a city in " + data[i].country + ".</p>" ;
+        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ".</p>" ;
     }
+
 cityContainer.insertAdjacentHTML('beforeend', htmlString );
 }
